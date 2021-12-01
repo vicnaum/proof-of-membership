8841f3da028d2846d3a006d422f6e6b44e9a2194
slvDev <slava.serbov@gmail.com>
2021-12-01 15:31:07 +0200

    decrease proof size

--- ui/zkp-ecdsa/src/zkpAttestList.ts
+++ ui/zkp-ecdsa/src/zkpAttestList.ts
@@ -86,6 +86,7 @@ function truncateToN(msg: bigint, n: bigint): bigint {
     return msg
 }
 
+// secLevel = 10 decrease proof size
 export function generateParamsList(secLevel = 80, curve: WeierstrassGroup = secp256k1): SystemParametersList {
     const nistGroup = generatePedersenParams(curve),
         proofGroup = generatePedersenParams(tomEdwards256)
@@ -132,7 +133,7 @@ export async function proveSignatureList(
         groupOrder = ec.order,
         z = truncateToN(fromBytes(msgHash), groupOrder),
         r = fromBytes(sigBytes.slice(0, len / 2)),
-        s = fromBytes(sigBytes.slice(len / 2, len-1)),
+        s = fromBytes(sigBytes.slice(len / 2, len - 1)),
         // First we do a signature verification to recover R
         sinv = invMod(s, groupOrder),
         u1 = posMod(sinv * z, groupOrder),
@@ -199,29 +200,29 @@ export async function verifySignatureList(
 export function hexStringToArrayBuffer(hexString) {
     // remove the leading 0x
     hexString = hexString.replace(/^0x/, "");
-  
+
     // ensure even number of characters
     if (hexString.length % 2 != 0) {
-      console.log(
-        "WARNING: expecting an even number of characters in the hexString"
-      );
+        console.log(
+            "WARNING: expecting an even number of characters in the hexString"
+        );
     }
-  
+
     // check for some non-hex characters
     var bad = hexString.match(/[G-Z\s]/i);
     if (bad) {
-      console.log("WARNING: found non-hex characters", bad);
+        console.log("WARNING: found non-hex characters", bad);
     }
-  
+
     // split the string into pairs of octets
     var pairs = hexString.match(/[\dA-F]{2}/gi);
-  
+
     // convert the octets to integers
     var integers = pairs.map(function (s) {
-      return parseInt(s, 16);
+        return parseInt(s, 16);
     });
-  
+
     var array = new Uint8Array(integers);
-  
+
     return array;
-  }
+}
